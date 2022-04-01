858830a8d9cf9695b7fc90690ddb3ac4c5a41f42
medal17 <iyandaadeshina@gmail.com>
2022-04-01 10:53:07 +0100

    correct stuff

--- pages/insight.js
+++ pages/insight.js
@@ -4,19 +4,36 @@ import HeadText from "../components/HeadText";
 import Navbar from "../components/Navbar";
 import InsightCard from "../components/Insights/InsightCard";
 // import Team from '../resources/images/phone.png'
-import pic0 from '../resources/images/article cover/0.png';
-import pic1 from '../resources/images/article cover/1.png';
-import pic2 from '../resources/images/article cover/2.png';
-import pic3 from '../resources/images/article cover/3.png';
-import pic8 from '../resources/images/article cover/8.png';
-import pic9 from '../resources/images/article cover/9.png';
-import pic10 from '../resources/images/article cover/10.png';
-import pic11 from '../resources/images/article cover/11.png';
+import pic0 from '../resources/images/articleCover/0.png';
+import pic1 from '../resources/images/articleCover/1.png';
+import pic2 from '../resources/images/articleCover/2.png';
+import pic3 from '../resources/images/articleCover/3.png';
+import pic4 from '../resources/images/articleCover/4.png';
+import pic5 from '../resources/images/articleCover/5.png';
+import pic6 from '../resources/images/articleCover/6.png';
+import pic7 from '../resources/images/articleCover/7.png';
+import pic8 from '../resources/images/articleCover/8.png';
+import pic9 from '../resources/images/articleCover/9.png';
+import pic10 from '../resources/images/articleCover/10.png';
+import pic11 from '../resources/images/articleCover/11.png';
 import Footer from "../components/Footer";
 import data from "../resources/data/articles.json"
 
 export default function Insights() {
-
+    const pics = [
+        {"name":pic0},
+        {"name":pic1},
+        {"name":pic2},
+        {"name":pic3},
+        {"name":pic4},
+        {"name":pic5},
+        {"name":pic6},
+        {"name":pic7},
+        {"name":pic8},
+        {"name":pic9},
+        {"name":pic10},
+        {"name":pic11},
+    ]
     return (
         <Grid container>
             <Navbar/>
@@ -29,7 +46,7 @@ export default function Insights() {
                     { data.map(e=>
                         <InsightCard
                         id ={e.id} 
-                        image={pic0} 
+                        image={pics[e.image].name} 
                         category='Business' 
                         date={e.author} 
                         title={e.title}
